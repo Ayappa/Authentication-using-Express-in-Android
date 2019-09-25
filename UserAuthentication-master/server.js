3addd7a0fb5db1a45a95f0314f7c4d21f04158f5
Ayappa Krishnappa <ayappaganesh1995@gmail.com>
2019-09-25 19:39:21 -0400

    final commit

---
+++ UserAuthentication-master/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const connectDB = require("./config/db");
+
+connectDB();
+app.get("/", (req, res) => {
+	res.json("hello");
+});
+
+app.use(express.json({ extended: false }));
+
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+	console.log(`server started on ${port}`);
+});
